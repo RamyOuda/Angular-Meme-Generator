3e2d50f0aa54ab4e0148185ec0880106c6c49452
Ramy Ouda <ramyouda@gmail.com>
2022-10-13 10:04:33 -0400

    Add confirmation to Clear All button

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IMeme } from '../meme/imeme';
 import { MemeService } from '../meme/meme.service';
 
@@ -7,7 +8,7 @@ import { MemeService } from '../meme/meme.service';
   styleUrls: ['./library.component.css'],
 })
 export class LibraryComponent implements OnInit {
-  constructor(private memeService: MemeService) {}
+  constructor(private memeService: MemeService, private router: Router) {}
 
   ngOnInit(): void {
     this.memeService.getMemes();
@@ -19,7 +20,12 @@ export class LibraryComponent implements OnInit {
   memes!: IMeme[];
 
   clearLocalStorage(): void {
-    this.memeService.restoreToDefault();
+    if (
+      confirm('Are you sure you want to delete all memes from the library?')
+    ) {
+      this.memeService.restoreToDefault();
+      this.router.navigate(['/home']);
+    }
   }
 
   removeMeme(): void {
